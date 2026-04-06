49cd628ee10b607786f6ebde7e48b4df66c21860
David Gillis <david@flipmine.com>
2026-04-06 18:53:34 +0000

    Add Project introspection class for version lifecycle

---
+++ spec/rakelib/project_spec.rb
@@ -0,0 +1,84 @@
+require "rake"
+require "tmpdir"
+require_relative "../../rakelib/project"
+
+RSpec.describe Project do
+  around do |example|
+    Dir.mktmpdir("project_spec") do |tmpdir|
+      Dir.chdir(tmpdir) { example.run }
+    end
+  end
+
+  before do
+    FileUtils.mkdir_p("lib/my_gem")
+    File.write("lib/my_gem.rb", "module MyGem; end\n")
+    File.write("lib/my_gem/version.rb", "module MyGem\n  VERSION = \"1.2.3\".freeze\nend\n")
+  end
+
+  after do
+    Object.send(:remove_const, :MyGem) if defined?(MyGem)
+  end
+
+  subject(:project) { described_class.new(Dir.pwd) }
+
+  describe "#name" do
+    it "discovers the gem name from the lib directory" do
+      expect(project.name).to eq("my_gem")
+    end
+  end
+
+  describe "#klass" do
+    it "returns the gem module constant" do
+      project.version
+      expect(project.klass).to eq(MyGem)
+    end
+  end
+
+  describe "#version" do
+    it "returns a Project::Version with the correct value" do
+      expect(project.version.value).to eq("1.2.3")
+    end
+
+    it "returns a Project::Version with the correct path" do
+      expect(project.version.path.to_s).to end_with("lib/my_gem/version.rb")
+    end
+  end
+
+  describe "#refresh_version!" do
+    it "re-reads the version from disk after a file change" do
+      project.version
+      File.write("lib/my_gem/version.rb", "module MyGem\n  VERSION = \"1.2.4\".freeze\nend\n")
+      project.refresh_version!
+      expect(project.version.value).to eq("1.2.4")
+    end
+  end
+
+  describe "#increment_version" do
+    it "returns the next patch version" do
+      expect(project.increment_version.value).to eq("1.2.4")
+    end
+  end
+
+  describe "#write_version!" do
+    it "writes the new version to disk" do
+      old_version = project.version
+      new_version = project.increment_version
+      project.write_version!(old_version, new_version)
+      content = File.read("lib/my_gem/version.rb")
+      expect(content).to include("1.2.4")
+      expect(content).not_to include("1.2.3")
+    end
+  end
+
+  context "when lib directory has no gem subdirectory" do
+    before do
+      FileUtils.rm_rf("lib/my_gem")
+      FileUtils.rm("lib/my_gem.rb")
+      File.write("lib/standalone.rb", "# no matching dir\n")
+    end
+
+    it "raises ProjectIntrospectionError" do
+      expect { project.name }.to raise_error(Project::ProjectIntrospectionError)
+    end
+  end
+end
